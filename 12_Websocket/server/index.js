bd85607ad237f0be38d1732d33c62c15fcae78e2
Lukas Kellerstein <lukas.kellerstein@gmail.com>
2022-02-10 17:23:21 +0100

    latest changes

--- 12_Websocket/server/index.js
+++ 12_Websocket/server/index.js
@@ -5,9 +5,15 @@ const wss = new WebSocket.Server({ port: 8085 });
 wss.on('connection', (ws) => {
     console.log('server', "new connection");
 
-    // ws.on('message', (data) => {
-    //     console.log('server', 'received: %s', data.toString());
-    // });
+    ws.on('message', (data) => {
+        console.log('server', 'received: %s', data.toString());
+
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        })
+    });
 
     console.log('server', "send message back to client after connect");
     ws.send('thanks for connection');
---
+++ 50_puppeteer/1/README.md
@@ -0,0 +1,10 @@
+
+
+We can remotely inspect page: http://localhost:9222/
+
+Get Websocket address to Chrome DevTools: http://127.0.0.1:9222/json/version
+
+
+Open new Chrome with new profile (remote-profile):
+`/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=remote-profile`
+
